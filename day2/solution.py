2bb489d68d9621ecfcc76405ee1f755d08536e08
Murat Eroglu <murat51392@gmail.com>
2021-12-02 17:57:25 +0300

    initial commit

---
+++ day2/solution.py
@@ -0,0 +1,23 @@
+with open('day2/input.txt') as f:q=f.readlines()
+x=y=0
+for i in q:
+    a,b = i.split()
+    if a=="forward":
+        x += int(b)
+    elif a=="up":
+        y -= int(b)
+    else:
+        y += int(b)
+print(f"part1: {x*y}")
+
+x=y=aim=0
+for i in q:
+    a,b = i.split()
+    if a=="forward":
+        x += int(b)
+        y += aim * int(b)
+    elif a=="up":
+        aim -= int(b)
+    else:
+        aim += int(b)
+print(f"part2: {x*y}")
