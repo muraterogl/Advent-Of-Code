a605613c0c55f5f237c4ab1110f456207a60dc1c
Murat Eroglu <murat51392@gmail.com>
2021-12-07 08:22:17 +0300

    day 7

---
+++ day7/solution.py
@@ -0,0 +1,9 @@
+with open('day7/input.txt') as f:q=f.readlines()
+
+*positions, = map(int, q[0].split(","))
+
+p1 = min(sum(abs(i-positions[j])for j in range(len(positions)))for i in range(max(positions)))
+print(f"part1: {p1}")
+
+p2 = int(min(sum(((u:=abs(i-positions[j]))*(u+1)/2)for j in range(len(positions)))for i in range(max(positions))))
+print(f"part2: {p2}")
