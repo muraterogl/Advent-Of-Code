01115dafcdcf96213a1e184f89dafc0415d053c2
Murat Eroğlu <70011058+muraterogl@users.noreply.github.com>
2021-12-23 23:39:10 +0300

    Update solution.py

--- day23/solution.py
+++ day23/solution.py
@@ -1,18 +1,21 @@
-from os import path
-from collections import Counter
-import numpy as np
-import re
 from copy import deepcopy
 import sys
 
 sys.setrecursionlimit(9**9)
 
-#demo
-# rooms = [["B","A"],["C","D"],["B","C"],["D","A"]]
+#demo part1
+#rooms = [["B","A"],["C","D"],["B","C"],["D","A"]]
 
-#real
-rooms = [["C","C"],["B","D"],["A","A"],["D","B"]]
-final = [["A","A"],["B","B"],["C","C"],["D","D"]]
+#demo part2
+#rooms = [["B","D","D","A"],["C","C","B","D"],["B","B","A","C"],["D","A","C","A"]]
+
+#real part1
+#rooms = [["C","C"],["B","D"],["A","A"],["D","B"]]
+#real part2
+rooms = [["C","D","D","C"],["B","C","B","D"],["A","B","A","A"],["D","A","C","B"]]
+
+#final = [["A","A"],["B","B"],["C","C"],["D","D"]]
+final = [["A","A","A","A"],["B","B","B","B"],["C","C","C","C"],["D","D","D","D"]]
 roomLen = len(rooms[0])
 waiting = [".",".",".",".",".",".","."]
 finalDest = {"A":0,"B":1,"C":2,"D":3,".":18293}
@@ -52,6 +55,20 @@ def findBetter(roomState, waitingState, cost):
                 if room[currentDepth]==".":
                     currentDepth+=1
                 else:
+                    #check if it can go to destination
+                    targetRoomIndex = finalDest[room[currentDepth]]
+                    targetRoomCorrectCount = roomState[targetRoomIndex].count(room[currentDepth])
+                    if targetRoomCorrectCount<roomLen and  all(cell=="." or cell==room[currentDepth] for cell in roomState[targetRoomIndex]):
+                        #check if corridor is empty
+                        if (targetRoomIndex < ix and all(waitingState[i]=="." for i in range(targetRoomIndex+2, ix+2))) or (targetRoomIndex > ix and all(waitingState[i]=="." for i in range(ix+2, targetRoomIndex+2))):
+                            roadLen = 2*abs(targetRoomIndex - ix) + 1 + currentDepth + roomLen - targetRoomCorrectCount
+                            nextRoomState = deepcopy(roomState)
+                            nextWaitingState = deepcopy(waitingState)
+                            nextCost = cost + costs[room[currentDepth]] * roadLen
+                            nextRoomState[ix][currentDepth] = "."
+                            nextRoomState[targetRoomIndex][roomLen-targetRoomCorrectCount-1] = room[currentDepth]
+                            possibleMoves.append((nextRoomState, nextWaitingState, nextCost))
+                            break
                     #left side of waiting
                     for i in range(waitingMinIndex,ix+2):
                         roadLen = currentDepth + 1 + 2*(ix+1-i) + (0 if i==0 else 1)
