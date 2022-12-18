8382e1202a74749da28b961144bf722c29caf3c0
Murat Eroğlu <70011058+muraterogl@users.noreply.github.com>
2022-12-18 13:15:16 +0300

    Update solution.py

--- 2022/day18/solution.py
+++ 2022/day18/solution.py
@@ -4,7 +4,7 @@ with open("input.txt") as f:
 cubes = {tuple(map(int,l.split(','))) for l in lines}
 sides = lambda x,y,z: {(x+1,y,z),(x-1,y,z),(x,y+1,z),(x,y-1,z),(x,y,z+1),(x,y,z-1)}
 
-print(sum((s not in cubes) for c in cubes for s in sides(*c)))
+print(f"Part1: {sum((s not in cubes) for c in cubes for s in sides(*c))}")
 
 seen = set()
 todo = [(-1,-1,-1)]
@@ -14,4 +14,4 @@ while todo:
     todo += [s for s in (sides(*here) - cubes - seen) if all(-1<=c<=25 for c in s)]
     seen |= {here}
 
-print(sum((s in seen) for c in cubes for s in sides(*c)))
+print(f"Part2: {sum((s in seen) for c in cubes for s in sides(*c))}")
